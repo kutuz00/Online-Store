e80b52fa65fbf0c58f88ed3d3fb760ea1ce59b1c
Kostyantyn <kutuz00@gmail.com>
2021-08-02 08:58:44 -0700

    Initial commit

--- Lesson-5/script.js
+++ Lesson-5/script.js
@@ -5,7 +5,7 @@ const app = new Vue({
         filteredItems: [],
         cart: [],
         searchLine: '',
-        isVisibleCart: false,
+        isVisibleCart: true,
         id_product: '',
     },
     methods: {
@@ -22,7 +22,7 @@ const app = new Vue({
         }
     },
     mounted() {
-        fetch('http://127.0.0.1:5500/Lesson-5/catalog.json').then(responce => responce.json())
+        fetch('http://127.0.0.1:500/Lesson-5/catalog.json').then(responce => responce.json())
             .then((responce) => this.items = responce)
             .then((responce) => this.filteredItems = responce);
     }
